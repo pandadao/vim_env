c637ff898336b6d847dc372c42770af1a31a5def
panda lover <cgsh112@gmail.com>
2017-10-01 10:52:40 +0800

    Add files via upload

---
+++ vim_env.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+git clone https://github.com/pandadao/my-vimrc.git
+
+cd my-vimrc/
+cp vimrc ~/.vimrc
+vim +PluginInstall +qall
+
